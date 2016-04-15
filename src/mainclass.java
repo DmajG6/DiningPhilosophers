5666e4b5803403ca25ed693ae80eb7d06714ddcd
TomKuric <TomKuric@HirFir>
2016-04-15 22:13:36 +0200

    Signed-off-by: TomKuric <TomKuric@HirFir>

--- src/mainclass.java
+++ src/mainclass.java
@@ -1,21 +1,21 @@
 public class mainclass {
 
-public static int X = 5; 
+	public static int X = 5; 
 
-public static Philosopher[] philosophers = new Philosopher[4];
+	public static Philosopher[] philosophers = new Philosopher[4];
 
-public static void main(String[] args) {
-    philosophers = new Philosopher[X];
-    ChopSticks chopsticks = new ChopSticks(X); 
-    Table table = new Table(X);
+	public static void main(String[] args) {
+		philosophers = new Philosopher[X];
+		ChopSticks chopsticks = new ChopSticks(X); 
+		Table table = new Table(X);
 
-    for(int i = 0;i<X;i++)
-        philosophers[i] = new Philosopher(i,100,200,300,1000,chopsticks,table);
+		for(int i = 0;i<X;i++)
+			philosophers[i] = new Philosopher(i,100,200,300,1000,chopsticks,table);
 
-    for(int i = 0;i<X;i++)
-        philosophers[i].start();
+		for(int i = 0;i<X;i++)
+			philosophers[i].start();
 
 
-}
+	}
 
 }
