40b0bc043b293c95d1c979574e92563f9489d528
TomKuric <tomkuric@gmail.com>
2016-05-04 09:06:37 +0200

    Signed-off-by: TomKuric <tomkuric@gmail.com>

--- src/Table.java
+++ src/Table.java
@@ -1,26 +1,27 @@
 class Table{
-private int isOccupied;
-private int x;
+	
+	private int isOccupied;
+	private int x;
 
-public Table(int X){
-    this.x = X;
-    isOccupied = x -1;
-}
+	public Table(int X){
+		this.x = X;
+		isOccupied = x -1;
+	}
 
-public synchronized void join(){
-    while(isOccupied == 0){
-        try{
-            wait();
-        }catch(InterruptedException e){             
-        }               
-    }
-     isOccupied--; 
-}
+	public synchronized void join(){
+		while(isOccupied == 0){
+			try{
+				wait();
+			}catch(InterruptedException e){             
+			}               
+		}
+		isOccupied--; 
+	}
 
-public synchronized void leave(){
+	public synchronized void leave(){
 
-    isOccupied++;
-    notify();
+		isOccupied++;
+		notify();
 
-}
+	}
 }
